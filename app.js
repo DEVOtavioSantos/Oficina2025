8a7696c2b37bf8d138a0832802ec4098a1c6e48a
Otavio Santos <85749511+DEVOtavioSantos@users.noreply.github.com>
2025-05-30 20:14:02 -0300

    Tentando fazer coretamente

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById('1').addEventListener('click', function()
+{
+    alert('Eai meu patrão');
+});
+
+document.getElementById('campo4').addEventListener('click', function()
+{
+    alert("Assim sim meu patrão");
+});
